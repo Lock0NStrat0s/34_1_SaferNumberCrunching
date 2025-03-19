82452feff2b0d3f5b4a992a9d3a2166a70c77334
Mohammad Jabir <mohammadj94@gmail.com>
2025-03-19 02:39:43 -0600

    challenge complete.

--- 34_SaferNumberCrunching/Program.cs
+++ 34_SaferNumberCrunching/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace _34_SaferNumberCrunching
+﻿namespace _34_SaferNumberCrunching;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int num;
+        do
         {
-            Console.WriteLine("Hello, World!");
-        }
+            Console.Write("Enter an int: ");
+        } while (!int.TryParse(Console.ReadLine(), out num));
+        Console.WriteLine(num);
+
+        double numD;
+        do
+        {
+            Console.Write("Enter a double: ");
+        } while (!double.TryParse(Console.ReadLine(), out numD));
+        Console.WriteLine(numD);
+
+        bool valB;
+        do
+        {
+            Console.Write("Enter a bool: ");
+        } while (!bool.TryParse(Console.ReadLine(), out valB));
+        Console.WriteLine(valB);
     }
 }
--- 34_SaferNumberCrunching/obj/Debug/net9.0/34_SaferNumberCrunching.AssemblyInfo.cs
+++ 34_SaferNumberCrunching/obj/Debug/net9.0/34_SaferNumberCrunching.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("34_SaferNumberCrunching")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11434785784e5823c77f6a2889a14c1b13ddf618")]
 [assembly: System.Reflection.AssemblyProductAttribute("34_SaferNumberCrunching")]
 [assembly: System.Reflection.AssemblyTitleAttribute("34_SaferNumberCrunching")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
